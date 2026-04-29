f6f088a62d7983c4b0aa13a85498098914783343
Sueda Yaşar <126615840+suedaa4@users.noreply.github.com>
2026-04-29 17:37:52 +0300

    Add files via upload

---
+++ JAVASCRIPT/conditional statements.js
@@ -0,0 +1,25 @@
+// if else
+
+var result = 30;
+if (result > 40) {
+    console.log("Congrats you passed.");
+} else {
+    console.log("Unfortunately you did not pass.");
+}
+
+
+// switch statement
+ var place = 'fourth';
+
+ switch(place) {
+    case 'first':
+        console.log('Gold');
+        break;
+    case 'second':
+        console.log('Silver');
+        break;
+    case 'third':
+        console.log('Bronze');
+    default:
+        console.log('No medal');
+ }
